abca7ab9b42127f4117992de08c296443f84014c
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-11-27 10:45:36 +0530

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+document.write("count the odd number od an array"+"<br>")
+document.write("--------------------------------"+"<br>")
+
+var odd=[21,23,45,77,73,22,98];
+var a=[];
+
+for(let i=0;i<=odd.length-1;i++){
+
+    if(odd[i]%2!==0){
+        a.push(odd[i])
+    }
+}
+document.write(a.length+"<br>")
